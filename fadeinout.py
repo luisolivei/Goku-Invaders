1fd91c4ba93138b05d1510743d94ee30d1ffb29f
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-11-13 10:06:13 +0000

    fadeinout
    Suavização de mudança de écrans

---
+++ fadeinout.py
@@ -0,0 +1,19 @@
+import pygame
+
+def fade_in_out(ecra, cor, largura, altura, tempo):
+    fade_surface = pygame.Surface((largura, altura))
+    fade_surface.fill(cor)
+
+    # Fade-in: aumenta a opacidade gradualmente
+    for alpha in range(0, 255, 5):  # Ajuste o passo (5) para a velocidade de fade desejada
+        fade_surface.set_alpha(alpha)
+        ecra.blit(fade_surface, (0, 0))
+        pygame.display.update()
+        pygame.time.delay(int(tempo / 51))  # Ajuste a duração conforme necessário
+
+    # Fade-out: diminui a opacidade gradualmente
+    for alpha in range(255, -1, -5):
+        fade_surface.set_alpha(alpha)
+        ecra.blit(fade_surface, (0, 0))
+        pygame.display.update()
+        pygame.time.delay(int(tempo / 51))
