0c1a66e41805d96300dde4398b823d107f296b4a
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-12-05 12:40:55 +0000

    Mudança de visual na tela game over
    Mudança de visual na tela game over

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ largura_ecra = 800
 altura_ecra = 600
 velocidade_animacao = 0.03  # Velocidade de troca de frames de animação
 velocidade_fundo = 0.8      # Velocidade de movimento do fundo
+caminho_fonte = "fonts/Dest.otf"  # Caminho da fonte vida score e nivel
