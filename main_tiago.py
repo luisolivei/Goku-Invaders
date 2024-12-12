904c4d0d01c7b4250a1515bb8ed948cd05bb1811
tbastosc <tbastosc@gmail.com>
2024-12-12 09:51:03 +0000

    resumo

--- main_tiago.py
+++ main_tiago.py
@@ -313,7 +313,7 @@ def pause_menu(ecra, fundo):
 def iniciar_jogo():
     global play
     ecra = pygame.display.set_mode((largura_ecra, altura_ecra))  # Inicializa a janela do menu
-    fundo = pygame.image.load("images/try1.jpg").convert_alpha()  # Carrega a imagem de fundo
+    fundo = pygame.image.load("images/imagem_inicial.jpg").convert_alpha()  # Carrega a imagem de fundo
     fundo = pygame.transform.smoothscale(fundo, (largura_ecra, altura_ecra))  # Redimensiona suavemente
     sons.tocar_musica_menu()
 
