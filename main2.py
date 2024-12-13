ddb7a6ad8214c62cd66fc3ed0755b507cf9a640d
luisolivei <107142551+luisolivei@users.noreply.github.com>
2024-12-13 12:00:44 +0000

    Abrir o txt ao carregar no highscore
    Abrir o txt ao carregar no highscore

--- main2.py
+++ main2.py
@@ -17,6 +17,39 @@ pontuacao = 0  # Variável para a pontuação
 nivel = 1  # Variável para o nível atual
 sons = Sons()  # Inicia o som
 
+def mostrar_highscore(ecra, fundo):
+    # Configuração do fundo
+    ecra.blit(fundo, (0, 0))
+    
+    try:
+        # Lê o arquivo de highscore
+        with open(arquivo_score, "r") as arquivo:
+            highscore = arquivo.read().strip()
+    except IOError:
+        highscore = "Nenhum highscore salvo."
+    
+    # Configuração do texto
+    fonte = pygame.font.Font(caminho_fonte, 48)
+    titulo = fonte.render("HIGHSCORE", True, (255, 255, 0))
+    texto_highscore = fonte.render(highscore, True, (255, 255, 0))
+    instrucoes = fonte.render("Pressione ESC para voltar", True, (200, 200, 200))
+    
+    # Centraliza o texto na tela
+    ecra.blit(titulo, (largura_ecra // 2 - titulo.get_width() // 2, altura_ecra // 4))
+    ecra.blit(texto_highscore, (largura_ecra // 2 - texto_highscore.get_width() // 2, altura_ecra // 2-80))
+    ecra.blit(instrucoes, (largura_ecra // 2 - instrucoes.get_width() // 2, altura_ecra - 100))
+    
+    pygame.display.update()
+    
+    # Aguarda o jogador pressionar ESC para voltar ao menu
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE:
+                return
+
 
 # Caminho para salvar o arquivo de score
 arquivo_score = "highscore.txt"
@@ -330,11 +363,15 @@ def iniciar_jogo():
     while True:
         highscore = carregar_highscore()
         # Exibe o menu inicial com título
-        escolha = menu(ecra, largura_ecra, altura_ecra, fundo, ["Play","{highscore}", "Quit"])
+        escolha = menu(ecra, largura_ecra, altura_ecra, fundo, ["Play","Highscore", "Quit"])
         if escolha == "Play":
             fade_in_out(ecra, (0, 0, 0), largura_ecra, altura_ecra, 20)
             play = True
             play_game()  # Inicia o jogo
+            fade_in_out(ecra, (0, 0, 0), largura_ecra, altura_ecra, 20)
+        elif escolha == "Highscore":
+            fade_in_out(ecra, (0, 0, 0), largura_ecra, altura_ecra, 20)
+            mostrar_highscore(ecra,fundo)
             fade_in_out(ecra, (0, 0, 0), largura_ecra, altura_ecra, 20)
         elif escolha == "Quit":
             fade_in_out(ecra, (0, 0, 0), largura_ecra, altura_ecra, 20)
