c29040c250cfddfb8d07cc6dfdcbf863975ffb3b
luisolivei <107142551+luisolivei@users.noreply.github.com>
2025-01-07 15:36:14 +0000

    Alteraçao de comentarios e na tela_final
    Retirado o titulo da tela final depois de 4 segundos e voltar ao menu 2 segundos depois de passar os creditos

--- main.py
+++ main.py
@@ -123,15 +123,15 @@ def play_game():
             inimigos.append(gerar_inimigo(nivel))
 
         # Lógica de níveis
-        if nivel == 1 and pontuacao >= 500:  # Nível 1: 800 pontos para avançar
+        if nivel == 1 and pontuacao >= 800:  # Nível 1: 800 pontos para avançar
             nivel += 1
             fundo = avancar_nivel(ecra, nivel, largura_ecra, altura_ecra, sons, jogador, inimigos)
 
-        elif nivel == 2 and pontuacao >= 1200:  # Nível 2: 2000 pontos para avançar
+        elif nivel == 2 and pontuacao >= 2000:  # Nível 2: 2000 pontos para avançar
             nivel += 1
             fundo = avancar_nivel(ecra, nivel, largura_ecra, altura_ecra, sons, jogador, inimigos)
 
-        elif nivel == 3 and pontuacao >= 2000:  # Nível 3: 3600 pontos para avançar
+        elif nivel == 3 and pontuacao >= 3600:  # Nível 3: 3600 pontos para avançar
             nivel += 1
             fundo = avancar_nivel(ecra, nivel, largura_ecra, altura_ecra, sons, jogador, inimigos)
 
@@ -351,7 +351,7 @@ def play_game():
 # Configuração inicial do menu
 def iniciar_jogo():
     global play
-    
+    pygame.display.set_caption("Goku Invaders")  # Define o título da janela
     ecra = pygame.display.set_mode((largura_ecra, altura_ecra))  # Inicializa a janela do menu
     fundo = pygame.image.load("imagens/Backgrounds/imagem_inicial.jpg").convert_alpha()  # Carrega a imagem de fundo
     fundo = pygame.transform.smoothscale(fundo, (largura_ecra, altura_ecra))  # Redimensiona suavemente
